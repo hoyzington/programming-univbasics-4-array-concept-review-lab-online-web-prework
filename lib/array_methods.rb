481b90c85ccab917e3388fa58b0f22508fe74d77
Scott Hoisington <hoyzington@gmail.com>
2019-11-01 08:06:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,15 +4,13 @@ def find_element_index(array, value_to_find)
       return nil
     elsif array[find] == value_to_find
       return find
-    
     end
   end
 end
 
 def find_max_value(array)
- 
-    
-  
+  array.sort!
+  puts array[array.length]
 end
 
 def find_min_value(array)
