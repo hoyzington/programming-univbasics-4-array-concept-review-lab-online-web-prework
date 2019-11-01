ef02356601a11bd41091cf333326f073a047ceb1
Scott Hoisington <hoyzington@gmail.com>
2019-11-01 07:57:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   array.length.times do |find|
-    if array.include?(value_to_find) = false
+    if array.include?(value_to_find) == false
       return nil
     elsif array[find] == value_to_find
       return find
