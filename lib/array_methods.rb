57da12b3c90fb775a0bbfbed0f54d37f9dba344c
Scott Hoisington <hoyzington@gmail.com>
2019-11-01 07:53:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,21 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |find|
+    if array[find] == value_to_find
+      return find
+    elsif array[find] == array.length
+      return nil
+    end
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+ 
+    
+  
 end
 
 def find_min_value(array)
-  # Add your solution here
+  
+    
+  
 end
